720ae6be30f20ab8281d2b3ed765d5a72b0232b4
OĞUZHAN KARA <oguz.kaan.kara@outlook.com>
2024-02-18 22:48:06 +0800

    StringFormating

--- Variables/main.go
+++ Variables/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
@@ -21,37 +20,39 @@ func main() {
 	//Education Detail : Tek bir print içerisine birden fazla değişken yazdırabilirsin.
 	//fmt.Println(productName, " ", allowAge, " ", discount, " ", isActive)
 
-	//
+	//fmt.Printf() : String formatlama
+	//\n : alt satıra geçer.
+	fmt.Printf("Product Name: %s\n Allow Age : %d\n Discount : %f, Status : %t", productName, allowAge, discount, isActive)
 
-	fmt.Println("Eğitimin ismi :", productName, reflect.TypeOf(productName))
-	fmt.Println("Hangi yaştaki öğrenciler girecek : ", allowAge, reflect.TypeOf(allowAge))
-	fmt.Println("indirim : ", discount, reflect.TypeOf(discount))
-	if isActive == true {
-		fmt.Println("Eyy oraz kursa kayıt olabilirsin")
-	} else {
-		fmt.Println("Lütfen kursun açılmasını takipediniz")
-	}
-	//reflect.TypeOf(productName) : typeof veri tipinin döndürür.
+	// fmt.Println("Eğitimin ismi :", productName, reflect.TypeOf(productName))
+	// fmt.Println("Hangi yaştaki öğrenciler girecek : ", allowAge, reflect.TypeOf(allowAge))
+	// fmt.Println("indirim : ", discount, reflect.TypeOf(discount))
+	// if isActive == true {
+	// 	fmt.Println("Eyy oraz kursa kayıt olabilirsin")
+	// } else {
+	// 	fmt.Println("Lütfen kursun açılmasını takipediniz")
+	// }
+	// //reflect.TypeOf(productName) : typeof veri tipinin döndürür.
 
-	//second  method
+	// //second  method
 
-	var deathVariables string = "Değişkenin ömrü bu sayfada global değil bu."
+	// var deathVariables string = "Değişkenin ömrü bu sayfada global değil bu."
 
-	println(deathVariables)
+	// println(deathVariables)
 
-	//third method
+	// //third method
 
-	var veritipiniKendiBelirleyenYontem = "Senin veri tipin string olsun. Dikkat et veri tipinin yanına bişey yazmadık.eğer buraya sayı yazsaydık int oalcaktı ama parantez dışında sayıy yazsaydık."
-	fmt.Println(veritipiniKendiBelirleyenYontem, reflect.TypeOf(veritipiniKendiBelirleyenYontem))
+	// var veritipiniKendiBelirleyenYontem = "Senin veri tipin string olsun. Dikkat et veri tipinin yanına bişey yazmadık.eğer buraya sayı yazsaydık int oalcaktı ama parantez dışında sayıy yazsaydık."
+	// fmt.Println(veritipiniKendiBelirleyenYontem, reflect.TypeOf(veritipiniKendiBelirleyenYontem))
 
-	//four metodh
-	varsızDegisken := ":= var yazmak istemiyorsan bu işareti koy"
-	fmt.Println(varsızDegisken)
+	// //four metodh
+	// varsızDegisken := ":= var yazmak istemiyorsan bu işareti koy"
+	// fmt.Println(varsızDegisken)
 
-	var intValue int8 = -123
-	fmt.Println(intValue)
+	// var intValue int8 = -123
+	// fmt.Println(intValue)
 
-	//uint  : sadece positif değerler.
-	//byte : pozitif çok küçük değerler
+	// //uint  : sadece positif değerler.
+	// //byte : pozitif çok küçük değerler
 
 }
