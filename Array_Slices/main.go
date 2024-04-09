3f96a83b5040e2e0240207eed578c7f4fe78d891
OĞUZHAN KARA <oguz.kaan.kara@outlook.com>
2024-04-10 02:13:19 +0800

    ArraySlice

--- Array_Slices/main.go
+++ Array_Slices/main.go
@@ -24,6 +24,36 @@ func main() {
 
 	// fmt.Println(categories[0:4])
 
+	// var jordans [6]string
+
+	// jordans[0] = "All Jordan"
+	// jordans[1] = "New Jordan"
+	// jordans[2] = "Jorden Shoes"
+	// jordans[3] = "Jorden 3"
+	// jordans[4] = "Jorden 4"
+	// jordans[5] = "Jorden 5"
+
+	// fmt.Println(jordans)      //tamamı yazıldı
+	// fmt.Println(jordans[0:4]) //kardeşim bana jordan 3 e kadar yazdır. bu veritabanından ayarla
+	// fmt.Println(jordans[1:4]) //kardeşim bana jordan 3 e kadar yazdır. bu veritabanından ayarla
+
+	var kids [8]string
+
+	kids[0] = "Infant & Toddler Shoes"
+	kids[1] = "Kids' Shoes"
+	kids[2] = "Kids' Jordan Shoes"
+	kids[3] = "Kids' Basketball Shoes"
+	kids[4] = "Kids' Running Shoes"
+	kids[5] = "Kids' Clothing"
+	kids[6] = "Kids' Backpacks"
+	kids[7] = "Kids' Backpacks"
+
+	fmt.Println(kids) //tamamı yazıldı
+
+	fmt.Println(jordans)      //tamamı yazıldı
+	fmt.Println(jordans[0:4]) //kardeşim bana jordan 3 e kadar yazdır. bu veritabanından ayarla
+	fmt.Println(jordans[1:4]) //kardeşim bana jordan 3 e kadar yazdır. bu veritabanından ayarla
+
 	// var gender = [3]string{"erkek", "kadın", "çocuk"}
 
 	// // fmt.Println(gender)
